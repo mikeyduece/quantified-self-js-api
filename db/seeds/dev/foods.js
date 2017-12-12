ab0923f19ee9cc8edf35fa8f6a0255058fa0a34a
snayrouz <samuel.nayrouz@gmail.com>
2017-12-12 09:23:03 -0700

    Change to resolve conflicts

--- db/seeds/dev/foods.js
+++ db/seeds/dev/foods.js
@@ -1,39 +1,17 @@
 exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
   return knex.raw('TRUNCATE foods RESTART IDENTITY')
-  .then(function() {
-    return Promise.all([
-      knex.raw(
-        'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)',
-        ["milkshake", 450, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)',
-        ["cereal", 200, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)',
-        ["cheetos", 100, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)',
-        ["pistachios", 180, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)',
-        ["burrito", 400, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)',
-        ["cookie", 150, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)',
-        ["kombucha", 50, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)',
-        ["nachos", 310, new Date]
-      )
-    ]);
-  });
+    .then(function() {
+        return knex('foods').insert([
+        {name: "Banana", calories: 150, created_at: new Date},
+        {name: "Bagel Bites - Four Cheese", calories: 650, created_at: new Date},
+        {name: "Chicken Burrito", calories: 800, created_at: new Date},
+        {name: "Grapes", calories: 180, created_at: new Date},
+        {name: "Blueberry Muffins",calories: 450, created_at: new Date},
+        {name: "Yogurt",calories: 550, created_at: new Date},
+        {name: "Macaroni and Cheese",calories: 950, created_at: new Date},
+        {name: "Granola Bar",calories: 200, created_at: new Date},
+        {name: "Sausage",calories: 9999, created_at: new Date}]
+        )
+    });
 };
