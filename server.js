dcda7476517c797a6b2f476565b4459e3b0add71
Mike Heft <mikeheft@gmail.com>
2017-12-13 14:10:43 -0700

    Remove Comments

--- server.js
+++ server.js
@@ -20,12 +20,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname+'/welcome.html'))
 })
