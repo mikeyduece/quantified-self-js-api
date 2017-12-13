e601d973fb80830ef438b3253ea1b98f4900196d
Mike Heft <mikeheft@gmail.com>
2017-12-13 13:39:46 -0700

    Add CORS fix

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.locals.title = 'Quantified Self'
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname+'/welcome.html'))
 })
