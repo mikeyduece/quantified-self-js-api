44d06418fb6d2ef28307ddd4a8558f11587378c8
Mike Heft <mikeheft@gmail.com>
2017-12-14 08:29:07 -0700

    Work on CORS whitelist

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.locals.title = 'Quantified Self'
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://snayrouz.github.io/quantified-self-starter-kit/*");
+  res.header("Access-Control-Allow-Origin", "https://snayrouz.github.io/quantified-self-starter-kit/");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
