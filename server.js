4dd0af6a8cc9ff153ef0c47e1176ee5840da94f4
Mike Heft <mikeheft@gmail.com>
2017-12-19 08:14:58 -0700

    Update allowable methods

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://mikeyduece.github.io");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
